2dd00190d76c95ecef1e0e59df1ad81bafc098a2
yb9255 <yoobin9255@gmail.com>
2025-03-16 19:33:26 +0900

    1,2,3 더하기 3 추가

---
+++ basic-first/dynamic-programming/ways-to-sum-using-1-2-and-3-3/index.js
@@ -0,0 +1,35 @@
+const input = require('fs')
+  .readFileSync('input.txt')
+  .toString()
+  .split('\n')
+  .map(Number);
+
+const iter = input.shift();
+
+/** 점화식
+ * 마지막에 1을 더하면 n - 1의 조합 개수에 1을 더한 것
+ * 마지막에 2를 더하면 n - 2의 조합 개수에 2를 더한 것
+ * 마지막에 3을 더하면 n - 3의 조합 개수에 3을 더한것
+ * dp[n] = dp[n - 1] + dp[n - 2] + dp[n - 3]
+ */
+
+const dp = [0, 1, 2, 4, 7];
+const answer = [];
+const MOD = 1_000_000_009;
+
+for (let i = 0; i < iter; i++) {
+  const num = input[i];
+
+  if (num < dp.length) {
+    answer.push(dp[num]);
+    continue;
+  }
+
+  for (let j = dp.length; j <= num; j++) {
+    dp[j] = (dp[j - 1] + dp[j - 2] + dp[j - 3]) % MOD;
+  }
+
+  answer.push(dp[num]);
+}
+
+console.log(answer.join('\n'));
