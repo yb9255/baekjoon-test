5abfed91ebbc54c6143bad89710dbbfcd347f5a5
yb9255 <yoobin9255@gmail.com>
2025-05-07 22:00:27 +0900

    로또 추가

---
+++ basic-second/backtracking/lotto/index.js
@@ -0,0 +1,35 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((str) => str.split(' ').map(Number));
+
+/** Pseudo Code
+ * 1. 조합은 이미 구한 값을 startIndex로 등록하여 startIndex 이후부터 순회를 돌도록 하여 구할 수 있음.
+ * 2. N과 M(10) 참조
+ */
+
+const MAX_DEPTH = 6;
+const answer = [];
+
+const getCombs = (startIndex, numbers, depth, stack) => {
+  if (depth === MAX_DEPTH) {
+    answer.push(stack.join(' '));
+    return;
+  }
+
+  for (let i = startIndex; i < numbers.length; i++) {
+    stack.push(numbers[i]);
+    getCombs(i + 1, numbers, depth + 1, stack);
+    stack.pop();
+  }
+};
+
+for (const [N, ...numbers] of input) {
+  if (N === 0) break;
+  getCombs(0, numbers, 0, []);
+  answer.push('');
+}
+
+console.log(answer.join('\n').trim());
