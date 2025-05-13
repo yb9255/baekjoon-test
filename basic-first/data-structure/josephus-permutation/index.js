b7b5ef67a9bf20dbdd1e91fc290fc8b56fb40525
yb9255 <yoobin9255@gmail.com>
2025-05-14 00:14:43 +0900

    요세푸스 문제 풀이에 원형 배열 추가

--- basic-first/data-structure/josephus-permutation/index.js
+++ basic-first/data-structure/josephus-permutation/index.js
@@ -1,7 +1,9 @@
-const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().trim().split(' ');
-
-const [iter, k] = input.map((val) => Number(val));
+const [iter, k] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
 
 const queue = Array.from({ length: iter }, (_, index) => index + 1);
 const answer = [];
@@ -47,3 +49,52 @@ while (queue2.length) {
 }
 
 console.log(`<${answer2.join(', ')}>`);
+
+/**  원형 배열 풀이(링크드 리스트 풀이)
+ * 1. 링크드 리스트를 바로 구현하기는 어려우므로 배열 두개로 만드는 링크드 리스트
+ * 2. 공간복잡도가 크므로 공간복잡도가 중요하면 사용하지 않는다.
+ *
+ * 3. prev = [], cur = [] 두개를 만들고,
+ *    prev[1] === N
+ *    prev[i] (0 < i <= n) === i - 1
+ *    next[n] === 1;
+ *    next[i] (0 < i <= n) === i + 1
+ *    형태로 링크드 리스트를 구현한다.
+ *
+ * 4. 이렇게하면 prev[n]에는 n의 이전값, next[n]에는 n의 이후값이 들어간다.
+ * 5. prev, next의 len을 기록한 다음, len이 끝날떄가지 순회하는 while문을 돌기 시작한다.
+ * 6. order가 k와 일치하지 않으면 order를 눌려준 다음 현재 값을 다음으로 넘긴다. (next[cur]);
+ * 7. order가 k와 일치하면, 링크드 리스트의 연결고리를 끊어야 한다. 다음 숫자의 prev를 현재 숫자의 prev로 바꾸고
+ * 다음 숫자의 next를 현재 숫자의 next로 바꿔 연결고리를 지운다. 이 후 order를 다시
+ */
+
+const prev = [];
+const next = [];
+const answer3 = [];
+
+for (let i = 1; i <= iter; i++) {
+  prev[i] = i === 1 ? iter : i - 1;
+  next[i] = i === iter ? 1 : i + 1;
+}
+
+let order = 1;
+let cur = 1;
+let len = iter;
+
+while (len) {
+  if (order === k) {
+    prev[next[cur]] = prev[cur];
+    next[prev[cur]] = next[cur];
+    answer3.push(cur);
+    order = 1;
+    len--;
+
+    cur = next[cur];
+  } else {
+    order++;
+    cur = next[cur];
+  }
+}
+
+// 출력
+console.log('<' + answer3.join(', ') + '>');
