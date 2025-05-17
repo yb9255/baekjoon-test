57bf688c0c1ec58a7d1b8d72ff754fa54706186f
yb9255 <yoobin9255@gmail.com>
2025-05-17 15:26:28 +0900

    카드 2 추가

---
+++ barkingdog/queue/card-2/index.js
@@ -0,0 +1,21 @@
+const N = +require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+  .toString()
+  .trim();
+
+/** Pseudo Code
+ * 1. frontIndex를 더해서 앞 카드를 버림
+ * 2. 가장 뒤 인덱스에 현재 frontIndex를 넣고, frontIndex 값을 1 늘림
+ * 3. node에서 shift 성능은 좋지 않으므로 two pointer 형태로 구현
+ */
+
+const queue = Array.from({ length: N }, (_, index) => index + 1);
+let frontIndex = 0;
+let backIndex = N;
+
+while (frontIndex < backIndex - 1) {
+  frontIndex++;
+  queue[backIndex++] = queue[frontIndex++];
+}
+
+console.log(queue[frontIndex]);
