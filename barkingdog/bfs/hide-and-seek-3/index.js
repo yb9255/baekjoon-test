65b290b5781b31bb98faf47e7622e5dc4dfeb66a
yb9255 <yoobin9255@gmail.com>
2025-06-03 18:02:12 +0900

    숨바꼭질 3 추가

---
+++ barkingdog/bfs/hide-and-seek-3/index.js
@@ -0,0 +1,61 @@
+const [seekerPosition, hiderPosition] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+/**
+ * Pseudo Code
+ *
+ * 1. 이동 거리를 함께 기록하는 visited 배열을 생성한다.
+ *
+ * 2. 현재 포지션에서 2의 제곱에 해당하는 모든 포지션에 현재 이동시간을
+ * 기록하고 queue에 넣는 함수 teleport를 만든다.
+ *
+ * 3. 현재 위치 2제곱을 하는 케이스를 계산하기 위해 시작 포지션의 visited를 1로 설정하고
+ * queue에 시작 포지션을 넣고 teleport 함수를 적용한다.
+ *
+ * 4. bfs를 시작한다. <현재 위치 + 1>, <현재 위치 - 1> 케이스를 추적 후 이동 범위 내에 있으며,
+ * 방문 했던 기록이 없다면 visited[다음 위치] = visited[현재 위치] + 1로 기록 후 queue에 push한다.
+ *
+ * 5. 최종적으로 hiderPosition과 seekerPosition이 같아지면, bfs를 종료하고 정답을 리턴한다.
+ */
+
+const LIMIT = 100_001;
+const visited = Array(LIMIT).fill(0);
+const queue = [seekerPosition];
+let front = 0;
+
+const teleport = (position) => {
+  if (position === 0) return;
+
+  let curPosition = position * 2;
+
+  while (curPosition < LIMIT && visited[curPosition] === 0) {
+    visited[curPosition] = visited[position];
+    queue.push(curPosition);
+    curPosition *= 2;
+  }
+};
+
+visited[seekerPosition] = 1;
+teleport(seekerPosition);
+
+while (front < queue.length) {
+  const position = queue[front++];
+
+  if (position === hiderPosition) {
+    console.log(visited[position] - 1);
+    break;
+  }
+
+  for (const next of [position + 1, position - 1]) {
+    if (next < 0 || next >= LIMIT) continue;
+    if (visited[next] !== 0) continue;
+
+    visited[next] = visited[position] + 1;
+    queue.push(next);
+    teleport(next);
+  }
+}
---
+++ barkingdog/bfs/hide-and-seek-3/index2.js
@@ -0,0 +1,45 @@
+const [seekerPosition, hiderPosition] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+/** Pseudo Code
+ * 1. deque에 쓸 배열을 만들고, 넉넉하게 앞뒤로 움직일 수 있도록 N의 두배 길이로 생성
+ * 2. 처음에 deque의 front/back 모두 중간에 위치시킨 뒤, N을 push_back으로 넣음
+ * 3. distance를 기록하는 배열을 만들고, seekerPosition을 0으로 기록
+ * 4. bfs로 deque를 순회, 처음에는 dequeue로 값을 꺼내온 뒤 teleport할 좌표를 구한다.
+ * 5. distance의 teleport 좌표에 현재 distance를 기록하고, teleport를 push_front로 넣음. 방문한 기록이 있으면 건너뜀.
+ * 6. 이 후 +-1씩 이동하는 좌표는 push_back으로 넣고 다음 좌표의 distance는 현재 좌표의 +1을 기록. 방문한 기록이 있으면 건너뜀
+ * 7. bfs에서 hiderPosition에 도달하면, distance[curSeekerPosition]을 리턴
+ */
+
+const MAX = 100_001;
+const distance = new Array(MAX).fill(-1);
+
+const deque = new Array(MAX * 2);
+let front = MAX;
+let back = MAX;
+
+deque[back++] = seekerPosition;
+distance[seekerPosition] = 0;
+
+while (front < back) {
+  const curSeekerPosition = deque[front++];
+
+  const teleport = curSeekerPosition * 2;
+
+  if (teleport < MAX && distance[teleport] === -1) {
+    distance[teleport] = distance[curSeekerPosition];
+    deque[--front] = teleport;
+  }
+
+  for (const next of [curSeekerPosition - 1, curSeekerPosition + 1]) {
+    if (next < 0 || next >= MAX || distance[next] !== -1) continue;
+    distance[next] = distance[curSeekerPosition] + 1;
+    deque[back++] = next;
+  }
+}
+
+console.log(distance[hiderPosition]);
