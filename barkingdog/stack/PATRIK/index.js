705c34865a346be07b406f093a4c57dc18331434
yb9255 <yoobin9255@gmail.com>
2025-07-12 20:56:34 +0900

    오아시스 재결합 설명 개선

--- barkingdog/stack/PATRIK/index.js
+++ barkingdog/stack/PATRIK/index.js
@@ -5,25 +5,22 @@ const [N, ...heights] = require('fs')
   .split('\n')
   .map(Number);
 
-/** Pseudo Code
- * 1. stack에 키를 담는다. stack[stack.length - 1]은
- * 현재 키인 사람이 볼 수 있는 가장 멀리 있는 사람이 된다.
+/**
+ * Pseudo code
+ * 1. for문으로 heights를 순회하면서 stack에 현재 높이와 같은 높이가 앞에 몇개가 있었는지 값을 넣는다.
  *
- * 2. heights를 순회한다.
- * 3. 현재 키를 가진 사람의 count가 1인 형태로 순회를 시작한다.
- * 4. 첫 순회에서 { height, count } 형태로 stack에 값을 push한다.
+ * 2. stack을 while문으로 뒤부터 순회하면서 stack의 top이 현재 높이보다 낮거나 같다면, 지금 누적된
+ * 같은 높이 개수만큼 쌍이 추가로 생기므로 정답 변수 pairCount에 top의 sameHeightCount를 더한다.
  *
- * 5. stack의 top이 현재 키보다 작을 경우 stack을 pop한 다음 페어 카운트를
- * count에 있는 사람 갯수만큼 올린다.
+ * 3. 만약 높이가 같다면, 같은 높이 stack이 한개 더 있음을 기록해야 하므로 sameHeightCount에
+ * 현재까지 누적된 top의 sameHeightCount를 더한다.
  *
- * 6. 만약 top의 키와 지금 키가 같다면, 다음 push에 들어갈 count를 1 올린다.
+ * 4. 만약 stack이 여전히 남아있다면, 현재 높이보다 더 큰 높이가 stack의 top 남아있다는 의미.
+ * 현재 높이는 키가 작아서 stack의 top 아래랑 마주볼 수 없으므로 stack의 top과만 볼 수 있으므로
+ * pairCount에 1만 더한다.
  *
- * 7. stack이 여전히 남아있다면 top의 사람과 현재 사람 사이에 stack의 top보다 큰 사람이
- * 없으므로 서로 바라볼 수 있으므로 페어 카운트를 1 올린다.
- *
- * 8. 다음 순회때, 만약 stack top의 카운트가 2이며 stack top의 키가 현재 키보다 작다면,
- * 2명과 마주볼 수 있으므로 top의 카운트만큼 페어 카운트가 늘어난다.
- *
+ * 5. 그 다음 자기 자신의 높이와 같은 높이 개수를 stack에 push한다.
+ * 6. 최종적으로 pairCount를 로그한다.
  */
 
 const stack = [];
@@ -31,22 +28,19 @@ let pairCount = 0;
 
 for (let i = 0; i < N; i++) {
   const height = heights[i];
-  let count = 1;
+  let sameHeightCount = 1;
 
-  while (stack.length && stack[stack.length - 1].height <= height) {
-    const top = stack.pop();
-    pairCount += top.count;
+  while (stack.length && stack[stack.length - 1][0] <= height) {
+    const [topHeight, topSameHeightCount] = stack.pop();
+    pairCount += topSameHeightCount;
 
-    if (top.height === height) {
-      count += top.count;
+    if (topHeight === height) {
+      sameHeightCount += topSameHeightCount;
     }
   }
 
-  if (stack.length) {
-    pairCount += 1;
-  }
-
-  stack.push({ height, count });
+  if (stack.length) pairCount++;
+  stack.push([height, sameHeightCount]);
 }
 
 console.log(pairCount);
