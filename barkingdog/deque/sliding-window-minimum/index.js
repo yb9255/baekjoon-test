61860e18af304bda6f6d02d9cecbf7a62daf8486
yb9255 <yoobin9255@gmail.com>
2025-10-27 13:58:09 +0900

    최솟값 찾기 코드 개선

--- barkingdog/deque/sliding-window-minimum/index.js
+++ barkingdog/deque/sliding-window-minimum/index.js
@@ -1,8 +1,11 @@
-const input = require('fs')
+/** https://www.acmicpc.net/problem/11003 */
+
+const [[N, L], nums] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .trim()
-  .split('\n');
+  .split('\n')
+  .map((line) => line.split(' ').map(Number));
 
 /** Pseudo Code
  * 1. 현재 값의 index와 value를 저장하는 deque을 생성
@@ -10,12 +13,10 @@ const input = require('fs')
  * 3. 그 다음 현재 값을 덱에 삽입한다.
  * 4. 덱의 앞부분의 index가 현재 window를 넘어갔다면 전부 shift로 제거한다.
  * 5. 1 ~ 4의 과정을 거치면 현재 덱의 0번 index의 값이 최소값이 되므로, 0번 index의 value를 결과 배열에 push 한다.
+ *
  * 6. 일반적인 console.log를 한번에 표현하면 node.js 사용 시 메모리 초과가 자꾸 발생하는 풀이.
  * 따라서 만개 단위로 stream하게 process.stdout으로 로그를 표시한다.
  */
-
-const [N, L] = input[0].split(' ').map(Number);
-const nums = input[1].split(' ').map(Number);
 
 const deque = new Array(N);
 let front = 0;
