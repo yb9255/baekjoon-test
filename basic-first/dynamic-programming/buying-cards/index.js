004018c4d9755882762ea7a6ffe6fd9470f17e76
yb9255 <yoobin9255@gmail.com>
2025-10-29 18:11:38 +0900

    카드 구매하기 코드 개선

--- basic-first/dynamic-programming/buying-cards/index.js
+++ basic-first/dynamic-programming/buying-cards/index.js
@@ -1,11 +1,13 @@
-const input = require('fs')
-  .readFileSync('input2.txt')
+/** https://www.acmicpc.net/problem/11052 */
+
+let [[N], prices] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .trim()
-  .split('\n');
+  .split('\n')
+  .map((line) => line.split(' ').map(Number));
 
-const n = Number(input[0]);
-const cardPackPrices = [0, ...input[1].split(' ').map(Number)];
+prices = [0, ...prices];
 
 /** 점화식
  * n개의 카드를 구매하기 위해 m개가 들어있는 카드팩을 살 때
@@ -15,24 +17,26 @@ const cardPackPrices = [0, ...input[1].split(' ').map(Number)];
  * 더 비싸며, m === n 일때.
  */
 
-// 0. n개의 카드를 사기 위한 최대 가격을 기록하는 DP 배열
-// 1. 1개의 카드부터 n개의 카드까지에 대응하는 for문 (cardsToBuy)을 돈다.
-// 2. 각 cardsToBuy 순회마다, m개의 카드팩을 샀을 때 최대값을 구하는 중첩 배열 (packSize)을 돈다
+/** Pseudo Code
+ * 0. n개의 카드를 사기 위한 최대 가격을 기록하는 DP 배열
+ * 1. 1개의 카드부터 n개의 카드까지에 대응하는 for문 (cardsToBuy)을 돈다.
+ * 2. 각 cardsToBuy 순회마다, m개의 카드팩을 샀을 때 최대값을 구하는 중첩 배열 (packSize)을 돈다
+ *
+ * 3. 각 packSize 순회마다, 점화식 1과 2의 케이스를 비교하면서 최대 가격을 maxPrice기록한다. 최종적으로
+ * n개의 카드를 사기 위해 1부터 n개가 들어있는 카드팩의 케이스 중 가장 큰 값을 구함
+ *
+ * 4. maxPrice[n]이 가장 비싼 카드값 케이스가 된다.
+ */
 
-// 3. 각 packSize 순회마다, 점화식 1과 2의 케이스를 비교하면서 최대 가격을 maxPrice기록한다. 최종적으로
-// n개의 카드를 사기 위해 1부터 n개가 들어있는 카드팩의 케이스 중 가장 큰 값을 구함
+const maxCost = Array(N + 1).fill(0);
 
-// 4. maxPrice[n]이 가장 비싼 카드값 케이스가 된다.
-
-const maxCost = Array.from({ length: n + 1 }, () => 0);
-
-for (let cardsToBuy = 1; cardsToBuy <= n; cardsToBuy++) {
+for (let cardsToBuy = 1; cardsToBuy <= N; cardsToBuy++) {
   for (let packSize = 1; packSize <= cardsToBuy; packSize++) {
     maxCost[cardsToBuy] = Math.max(
       maxCost[cardsToBuy],
-      maxCost[cardsToBuy - packSize] + cardPackPrices[packSize],
+      maxCost[cardsToBuy - packSize] + prices[packSize]
     );
   }
 }
 
-console.log(maxCost[n]);
+console.log(maxCost[N]);
