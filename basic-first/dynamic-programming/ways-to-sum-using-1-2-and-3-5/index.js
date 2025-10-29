443f601e9bc0fdd2a07f4fbfb678931d2390759d
yb9255 <yoobin9255@gmail.com>
2025-10-29 18:38:57 +0900

    1,2,3 더하기 5 풀이 개선

--- basic-first/dynamic-programming/ways-to-sum-using-1-2-and-3-5/index.js
+++ basic-first/dynamic-programming/ways-to-sum-using-1-2-and-3-5/index.js
@@ -1,5 +1,7 @@
-const input = require('fs')
-  .readFileSync('input.txt')
+/** https://www.acmicpc.net/problem/15990 */
+
+const [T, ...nums] = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .trim()
   .split('\n')
@@ -7,9 +9,7 @@ const input = require('fs')
 
 const MOD = 1_000_000_009;
 
-const iter = input.shift();
-
-/** 점화식
+/** Pseudo Code
  * 1. 마지막에 1을 더하는 경우는 현재 수보다 1 작고, 2 또는 3으로 끝나는 경우임
  * 2. 마지막에 2를 더하는 경우는 현재 수보다 2 작고, 1 또는 3으로 끝나는 경우임
  * 3. 마지막에 3을 더하는 경우는 현재 수보다 3 작고, 1 또는 2로 끝나는 경우임
@@ -28,25 +28,25 @@ const iter = input.shift();
  */
 
 const dp = [
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 1, 0],
-  [0, 1, 1, 1],
+  [0, 0, 0],
+  [1, 0, 0],
+  [0, 1, 0],
+  [1, 1, 1],
 ];
 
 const answer = [];
 
-for (let i = 0; i < iter; i++) {
-  const number = input[i];
+for (let t = 0; t < T; t++) {
+  const num = nums[t];
 
-  for (let j = dp.length; j <= number; j++) {
-    dp[j] = [];
-    dp[j][1] = (dp[j - 1][2] + dp[j - 1][3]) % MOD;
-    dp[j][2] = (dp[j - 2][1] + dp[j - 2][3]) % MOD;
-    dp[j][3] = (dp[j - 3][1] + dp[j - 3][2]) % MOD;
+  for (let i = dp.length; i <= num; i++) {
+    dp[i] = [];
+    dp[i][0] = (dp[i - 1][1] + dp[i - 1][2]) % MOD;
+    dp[i][1] = (dp[i - 2][0] + dp[i - 2][2]) % MOD;
+    dp[i][2] = (dp[i - 3][0] + dp[i - 3][1]) % MOD;
   }
 
-  answer.push((dp[number][1] + dp[number][2] + dp[number][3]) % MOD);
+  answer.push((dp[num][0] + dp[num][1] + dp[num][2]) % MOD);
 }
 
 console.log(answer.join('\n'));
