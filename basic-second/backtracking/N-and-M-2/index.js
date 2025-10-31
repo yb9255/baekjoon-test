3718a1482fd9bbbb5f068b09f7a78c7a57d65048
yb9255 <yoobin9255@gmail.com>
2025-10-31 17:02:47 +0900

    N과 M(2) 코드 개선

--- basic-second/backtracking/N-and-M-2/index.js
+++ basic-second/backtracking/N-and-M-2/index.js
@@ -1,5 +1,7 @@
+/** https://www.acmicpc.net/problem/15650 */
+
 const [N, M] = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input3.txt')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .split(' ')
   .map(Number);
--- basic-second/backtracking/N-and-M-2/index2.js
+++ basic-second/backtracking/N-and-M-2/index2.js
@@ -1,12 +1,14 @@
+/** https://www.acmicpc.net/problem/1182 */
+
 const [N, M] = require('fs')
-  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input2.txt')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input3.txt')
   .toString()
   .trim()
   .split(' ')
   .map(Number);
 
 /**
- * Pseudo Code
+ * Pseudo Code (BarkingDog 풀이)
  * 1. 현재 인덱스의 숫자를 계속 덮어씌우는 재귀를 돈다.
  * 2. 만약 idx가 0이 아니라면 idx 이전 인덱스부터 for문을 돈다.
  */
@@ -14,18 +16,18 @@ const [N, M] = require('fs')
 const curSequence = [];
 const answer = [];
 
-const generateSequence = (idx) => {
-  if (idx === M) {
+const generateSequence = (depth) => {
+  if (depth === M) {
     answer.push(curSequence.join(' '));
     return;
   }
 
   let start = 1;
-  if (idx !== 0) start = curSequence[idx - 1] + 1;
+  if (depth !== 0) start = curSequence[depth - 1] + 1;
 
-  for (let i = idx; i <= N; i++) {
-    curSequence[idx] = i;
-    generateSequence(idx + 1);
+  for (let i = start; i <= N; i++) {
+    curSequence[depth] = i;
+    generateSequence(depth + 1);
   }
 };
 
