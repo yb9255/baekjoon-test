1a2ed3244849e1baaa105feb019fb6c422126640
yb9255 <yoobin9255@gmail.com>
2026-01-19 12:21:43 +0900

    숨바꼭질 3 풀이 개선

--- barkingdog/bfs/hide-and-seek-3/index2.js
+++ barkingdog/bfs/hide-and-seek-3/index2.js
@@ -1,9 +1,15 @@
+/** https://www.acmicpc.net/problem/13549 */
+
 const [seekerPosition, hiderPosition] = require('fs')
   .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .trim()
   .split(' ')
   .map(Number);
+
+/** 0-1 BFS: 간선의 가중치가 0 또는 1인 BFS
+ * 이 경우 큐가 아닌 덱을 사용하는 BFS가 추천됨
+ */
 
 /** Pseudo Code
  * 1. deque에 쓸 배열을 만들고, 넉넉하게 앞뒤로 움직일 수 있도록 N의 두배 길이로 생성
@@ -13,6 +19,7 @@ const [seekerPosition, hiderPosition] = require('fs')
  * 5. distance의 teleport 좌표에 현재 distance를 기록하고, teleport를 push_front로 넣음. 방문한 기록이 있으면 건너뜀.
  * 6. 이 후 +-1씩 이동하는 좌표는 push_back으로 넣고 다음 좌표의 distance는 현재 좌표의 +1을 기록. 방문한 기록이 있으면 건너뜀
  * 7. bfs에서 hiderPosition에 도달하면, distance[curSeekerPosition]을 리턴
+ *
  */
 
 const MAX = 100_001;
@@ -28,6 +35,11 @@ distance[seekerPosition] = 0;
 while (front < back) {
   const curSeekerPosition = deque[front++];
 
+  if (curSeekerPosition === hiderPosition) {
+    console.log(distance[hiderPosition]);
+    break;
+  }
+
   const teleport = curSeekerPosition * 2;
 
   if (teleport < MAX && distance[teleport] === -1) {
@@ -41,5 +53,3 @@ while (front < back) {
     deque[back++] = next;
   }
 }
-
-console.log(distance[hiderPosition]);
